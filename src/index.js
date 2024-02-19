f69d5d21b75ddf29ce43e436c679b55be52d978d
your_github_username <your_github_email>
2024-02-19 08:28:11 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@
 
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
   path: './env'
 })
 
 
-connectDB()
+connectDB().then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at Port ${process.env.PORT}`);
+  })
+}).catch(err => {
+  console.log("monogo db connection failed !!! ", err);
+})
 
 
 
