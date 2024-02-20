e6e3c006f6ce6a2a355e71da1ac30f6a893bed39
a <a>
2024-02-20 16:28:28 +0530

    create user controller file in which call the already created method asyncHandler needs to pass parameter as a callback function , created user rotuer which pass the parameter call back funtion retisterUser

--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // configuration
 
-const app = express()
+const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({
-  limit: '16kb'
-}))
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 
-app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(express.static("pulbic"))
+app.use(express.static("pulbic"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-export { app }
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes decleration
+
+app.use("/api/v1/users", userRouter);
+
+export { app };
