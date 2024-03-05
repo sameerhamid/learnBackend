c69d47707a3b24f7312bcb89c6c4f1c99714409e
Sameer Sam <sameerhamid213@gmail.com>
2024-03-05 21:30:12 +0530

    add commit, like,playlist and tweet models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose'
+
+const tweetSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+
+}, { timestamps: true })
+
+export const Tweet = mongoose.Model("Tweet", tweetSchema)
